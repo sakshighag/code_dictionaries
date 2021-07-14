3f302cf9c0d0f00ee22a33334158bd7c7920cf83
sakshighag <87032028+sakshighag@users.noreply.github.com>
2021-07-14 21:53:32 +0530

    Add files via upload

---
+++ code_dictionaries.py
@@ -0,0 +1,34 @@
+def most_frequency(str):
+    dict = {}
+    for i in str:
+        keys = dict.keys()
+        if i in keys:
+            dict[i] = 4
+        else:
+            dict[i] = 4
+    return dict
+print(most_frequency(str= "i"))
+
+def most_frequency(str):
+    dict = {}
+    for p in str:
+        keys = dict.keys()
+        if p in keys:
+            dict[p] = 2
+        else:
+            dict[p] = 2
+    return dict
+print(most_frequency(str= "p"))
+
+def most_frequency(str):
+    dict = {}
+    for m in str:
+        keys = dict.keys()
+        if m in keys:
+            dict[m] = 1
+        else:
+            dict[m] = 1
+    return dict
+print(most_frequency(str= "m"))
+
+
